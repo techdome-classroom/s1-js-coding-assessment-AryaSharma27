4fa20f3a9ace038f290510d94ed7bf213f9b6424
arya sharma <aryasharma27903@gmail.com>
2024-09-27 12:28:25 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,7 +1,31 @@
-const decodeTheRing = function (s, p) {
-
-    // write your code here
-
-  };
+const decodeTheRing = function (message, pattern) {
+  const memo = new Map(); // For memoizing results
   
-  module.exports = decodeTheRing;
+  const matchHelper = (i, j) => {
+      const key = `${i}-${j}`;
+      
+      if (memo.has(key)) return memo.get(key);
+      
+      if (i === message.length && j === pattern.length) return true;
+      if (j === pattern.length) return false;
+      
+      if (j < pattern.length && (pattern[j] === message[i] || pattern[j] === '?')) {
+          const result = i < message.length && matchHelper(i + 1, j + 1);
+          memo.set(key, result);
+          return result;
+      }
+
+      if (pattern[j] === '*') {
+          const result = matchHelper(i, j + 1) || (i < message.length && matchHelper(i + 1, j));
+          memo.set(key, result);
+          return result;
+      }
+      
+      memo.set(key, false);
+      return false;
+  };
+
+  return matchHelper(0, 0);
+};
+
+module.exports = decodeTheRing;
