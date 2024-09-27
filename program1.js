ba3ce2280ea2eb0d32492c657d3c25fb957d41ce
arya sharma <aryasharma27903@gmail.com>
2024-09-27 12:21:51 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,4 +1,33 @@
 const getTotalIsles = function (grid) {
+  const getTotalIsles = function (grid) {
+    if (!grid || grid.length === 0) return 0;
+
+    const dfs = (grid, i, j) => {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] === 'W') {
+            return;
+        }
+        grid[i][j] = 'W'; // Mark the landmass as visited
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    };
+
+    let islandCount = 0;
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            if (grid[i][j] === 'L') {
+                islandCount++;
+                dfs(grid, i, j);
+            }
+        }
+    }
+
+    return islandCount;
+};
+
+module.exports = getTotalIsles;
+
 
 
 };
